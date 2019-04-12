8174faf72a0822363c0176391d2c4a485b507296
willy <xj6jou4xj6jo@gmail.com>
2019-04-12 18:02:19 +0800

    no message

--- test_api.py
+++ test_api.py
@@ -54,7 +54,8 @@ def callback():
 def handle_message(event):
     print(event)
     text = event.message.text
-    user_ID = event.source["userId"]
+    print(event.source)
+    user_ID = event.source
 	
     user_profile = line_bot_api.get_profile(user_ID)
 
